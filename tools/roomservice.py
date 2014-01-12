61cc548519edd0b784c6b8f479d18c80182fd4e4
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2014-01-12 00:14:31 -0600

    Update roomservice.py

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -35,16 +35,16 @@ except ImportError:
 
 # Config
 # set this to the default remote to use in repo
-default_rem = "anime"
+default_rem = "animerom"
 # set this to the default revision to use (branch/tag name)
 default_rev = "android-4.4"
 # set this to the remote that you use for projects from your team repos
 # example fetch="https://github.com/omnirom"
-default_team_rem = "AnimeROM"
+default_team_rem = "animerom"
 # this shouldn't change unless google makes changes
 local_manifest_dir = ".repo/local_manifests"
 # change this to your name on github (or equivalent hosting)
-android_team = "AnimeROM"
+android_team = "animerom"
 
 
 def check_repo_exists(git_data):
@@ -294,5 +294,5 @@ if __name__ == '__main__':
         deps_only = False
 
     if not deps_only:
-        fetch_device(device)
+    fetch_device(device)
     fetch_dependencies(device)
