6acc35665de5e8b87193f787b516df626e93d039
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2014-01-11 18:16:34 -0600

    Renamed to Anime

--- envsetup.sh
+++ envsetup.sh
@@ -59,8 +59,8 @@ function check_product()
         return
     fi
 
-    if (echo -n $1 | grep -q -e "^omni_") ; then
-       CUSTOM_BUILD=$(echo -n $1 | sed -e 's/^omni_//g')
+    if (echo -n $1 | grep -q -e "^anime_") ; then
+       CUSTOM_BUILD=$(echo -n $1 | sed -e 's/^anime_//g')
     else
        CUSTOM_BUILD=
     fi
@@ -477,7 +477,7 @@ function breakfast()
     CUSTOM_DEVICES_ONLY="true"
     unset LUNCH_MENU_CHOICES
     add_lunch_combo full-eng
-    for f in `/bin/ls vendor/omni/vendorsetup.sh 2> /dev/null`
+    for f in `/bin/ls vendor/Anime/vendorsetup.sh 2> /dev/null`
         do
             echo "including $f"
             . $f
