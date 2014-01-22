f92fff0329ec03e77b2da858f9bcb1968113991a
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2014-01-22 00:08:53 -0600

    selinux policies

--- core/config.mk
+++ core/config.mk
@@ -495,7 +495,7 @@ RS_PREBUILT_COMPILER_RT := prebuilts/sdk/renderscript/lib/$(TARGET_ARCH)/libcomp
 ifneq ($(CUSTOM_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include vendor/omni/sepolicy/sepolicy.mk)
+$(eval include vendor/Anime/sepolicy/sepolicy.mk)
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
